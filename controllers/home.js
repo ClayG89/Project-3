6e6641239d014b591ac03be584fdfe308546953c
ClayGuess <guessclay@Yahoo.com>
2020-03-11 19:42:43 -0400

    added apis

---
+++ controllers/home.js
@@ -0,0 +1,48 @@
+
+const express = require('express')
+
+
+const Home = require('../models/Home.js')
+
+
+const homeRouter = express.Router()
+
+
+homeRouter.get('/', (req, res) => {
+    Home.find().then((homes) => {
+        res.json(homes);
+    });
+});
+
+homeRouter.get('/:homeId', (req, res) => {
+    Home.findById(req.params.homeId).then((home) => {
+        res.json(home);
+    });
+});
+
+homeRouter.post('/', (req, res) => {
+    Home.create(req.body).then(() => {
+        res.status(200).end();
+    });
+});
+
+homeRouter.put('/:homeId', (req, res) => {
+    Home.findByIdAndUpdate(req.params.homeId, req.body).then(() => {
+        res.status(200).end();
+    });
+});
+
+homeRouter.delete('/:homeeId', (req, res) => {
+    Homee.findByIdAndDelete(req.params.homeId).then(() => {
+        res.status(200).end();
+    });
+});
+
+homeRouter.get('/', (req, res) => {
+  res.json('hello world')
+})
+
+
+module.exports = {
+  homeRouter
+}
